ecd9f45b1475eb9a3c54ea2e3d8a3f8282588caa
Glebych90 <147382234+Glebych90@users.noreply.github.com>
2024-12-19 22:03:29 +0300

    Add files via upload

---
+++ GITAlexandrov/GITAlexandrov/ClassFolder/MBClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GITAlexandrov.ClassFolder
+{
+    internal class MBClass
+    {
+        public static void ErrorMB(string text)
+        {
+            MessageBox.Show(text, "Ошибка",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+    }
+}
